a83bdf883ce2a0ab6c9366231b0794b04a46e955
Erel Dekel <ereldekel123321@gmail.com>
2024-05-31 15:23:06 +0300

    adding reward_gen class

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from game import SokobanGame
 from model import Agent
 import matplotlib.pyplot as plt
 
-#import queue
 import numpy as np
-from collections import deque
+from reward_gen import *
 
 def process_state(map_info, reshape=True):  # Take the game information and transform it into a stateS
     state = map_info[1:-1]  # Cut the frame
@@ -16,80 +15,8 @@ def process_state(map_info, reshape=True):  # Take the game information and tran
     
     return state
 
-# Find all path lengths to target
-def bfs(_map, target_x, target_y):
-    rows, cols = _map.shape
-    distances = np.full_like(_map, np.inf)
-    distances[target_y, target_x] = 0
-    queue = deque([(target_y, target_x)])
-
-    while queue:
-        current_node = queue.popleft()
-        i, j = current_node
-
-        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            new_i, new_j = i + di, j + dj
-            if (0 <= new_i < rows and 0 <= new_j < cols and _map[new_i, new_j] != 1 and distances[new_i, new_j] > distances[current_node] + 1):
-                distances[new_i, new_j] = distances[current_node] + 1
-                queue.append((new_i, new_j))
-
-    return distances
-
-
-def calculate_reward(state, action, next_state, done, distances, alpha): # Cuclulate the reward of a step basted on queue of the next steps
-    global loop_counter
-    if (state == next_state).all(): # If the agent chose wasteful action
-        return reward_for_waste
-    if done:  # If the agent finished the game
-        return reward_for_done
-
-    if check_loop(next_state, state_queue):
-        loop_counter += 1
-        change_loop_rewards(state_queue)
-        fill_none(state_queue)
-        return reward_for_loop
-
-    # for i, item in enumerate(state_queue):
-    #     if (next_state == item).all():
-    #         change_loop_rewards(i)
-    #         loop_counter += 1
-    #         return reward_for_loop
-
-    #Reward for each step for inefficiency with regard to distance from 
-    #distance_relation = alpha * (np.max(distances) / distances[env.cargo_y - 1,env.cargo_x - 1])
-    #distance_cargo_keeper = alpha * np.max(distances) / (np.abs(env.cargo_y - env.y) + np.abs(env.cargo_y - env.y))
-    return reward_for_move
-
-def check_loop(state, state_queue):
-    for s in state_queue:
-        if (s is not None) and (state == s).all():
-            return True
-    
-    return False
-
-def change_loop_rewards(state_queue):
-    loop_length = None
-    for i in range(len(state_queue)):
-        if state_queue[i] is None:
-            loop_length = i
-            break
-    
-    if loop_length is None:
-        return
-    
-    for i in range(loop_length):    
-        agent.replay_buffer[i][2] = reward_for_loop * (reward_for_loop_decay ** (i + 1))
-    
-    # for i in range(num):
-    #     agent.replay_buffer[i][2] += reward_for_loop
-
-def fill_none(state_queue):
-    state_queue.clear()
-    for _ in range(state_queue_length):
-        state_queue.appendleft(None)
-
 # init environment (game)
-env = SokobanGame(level=61, graphics_enable=False)
+env = SokobanGame(level=61, graphics_enable=True)
 
 # init agent
 agent_hyperparameters = {
@@ -102,8 +29,10 @@ agent_hyperparameters = {
 }
 agent = Agent(**agent_hyperparameters)
 
+reward_gen = Move_Done_Loop()
+
 # training parameters
-max_episodes = 10000
+max_episodes = 1000
 max_steps = 30
 
 successes_before_train = 10
@@ -113,30 +42,14 @@ continuous_successes = 0
 steps_per_episode = []
 target_rate = 5
 
-# base reward parameters
-reward_for_stuck = 0
-reward_for_waste = -2
-reward_for_done = 10
-reward_for_move = -0.5
-reward_for_loop = -4
-reward_for_loop_decay = 0.75
-
-state_queue_length = 5
-state_queue = deque(maxlen=state_queue_length)
-loop_counter = 0
 loops_per_episode = []
-
-init_state = process_state(env.map_info, reshape=False)
-distances = bfs(init_state, env.target_x - 1, env.target_y - 1)
-alpha = 0.2 # reward by distance multiplyer
 
 for episode in range(1, max_episodes + 1):
     if continuous_successes >= continuous_successes_goal:
         print("Agent training finished!")
         break
     
-    loop_counter = 0
-    fill_none(state_queue)
+    reward_gen.reset()
 
     print(f"Episode {episode} Epsilon {agent.epsilon:.4f}")
     env.reset_level()
@@ -147,11 +60,8 @@ for episode in range(1, max_episodes + 1):
         done = env.step_action(action=action)
         next_state = process_state(env.map_info)
 
-        reward = calculate_reward(state, action, next_state, done, distances, alpha)
+        reward = reward_gen.calculate_reward(state, next_state, done, agent.replay_buffer)
         agent.store_replay(state, action, reward, next_state, done)
-
-        state_queue.pop()
-        state_queue.appendleft(next_state)
 
         if successful_episodes >= successes_before_train:
             agent.replay()
@@ -167,7 +77,7 @@ for episode in range(1, max_episodes + 1):
             break
     
     #print(f'number of loops in episode {episode} is {loop_counter}')
-    loops_per_episode.append(loop_counter)
+    loops_per_episode.append(reward_gen.loop_counter)
 
     if not done:
         continuous_successes = 0
---
+++ reward_gen.py
@@ -0,0 +1,92 @@
+from collections import deque
+import zope.interface
+
+class Reward_Gen(zope.interface.Interface):
+    def calculate_reward(*arg, **kargs):
+        pass
+    def reset(*arg, **kargs):
+        pass
+
+
+@zope.interface.implementer(Reward_Gen)
+class Move_Done_Loop:
+    def __init__(self) -> None:
+        self.reward_for_waste = -2
+        self.reward_for_done = 10
+        self.reward_for_move = -0.5
+        self.reward_for_loop = -4
+        self.reward_for_loop_decay = 0.75
+        self.state_queue_length = 5
+        self.state_queue = deque(maxlen=self.state_queue_length)
+        self.loop_counter = 0
+
+    def calculate_reward(self, state, next_state, done, replay_buffer): # Cuclulate the reward of a step basted on queue of the next steps
+        reward = self.reward_for_move
+        if (state == next_state).all(): # If the agent chose wasteful action
+            reward = self.reward_for_waste
+        elif done:  # If the agent finished the game
+            reward = self.reward_for_done
+
+        elif self._check_loop(next_state):
+            self.loop_counter += 1
+            self._change_loop_rewards(replay_buffer)
+            self._fill_none()
+            reward = self.reward_for_loop
+        
+        self.state_queue.pop()
+        self.state_queue.appendleft(next_state)
+        return reward
+    
+    def _check_loop(self, state):
+        for s in self.state_queue:
+            if (s is not None) and (state == s).all():
+                return True
+        
+        return False
+
+    def _change_loop_rewards(self, replay_buffer):
+        loop_length = None
+        for i in range(len(self.state_queue)):
+            if self.state_queue[i] is None:
+                loop_length = i
+                break
+        
+        if loop_length is None:
+            return
+        
+        for i in range(loop_length):    
+            replay_buffer[i][2] = self.reward_for_loop * (self.reward_for_loop_decay ** (i + 1))
+
+    def reset(self):
+        self.loop_counter = 0
+        self._fill_none()
+    
+    def _fill_none(self):
+        self.state_queue.clear()
+        for _ in range(self.state_queue.maxlen):
+            self.state_queue.appendleft(None)
+
+@zope.interface.implementer(Reward_Gen)
+class BFS:
+    def calculate_reward(self, _map, target_x, target_y):
+        # Find all path lengths to target
+        # rows, cols = _map.shape
+        # distances = np.full_like(_map, np.inf)
+        # distances[target_y, target_x] = 0
+        # queue = deque([(target_y, target_x)])
+
+        # while queue:
+        #     current_node = queue.popleft()
+        #     i, j = current_node
+
+        #     for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        #         new_i, new_j = i + di, j + dj
+        #         if (0 <= new_i < rows and 0 <= new_j < cols and _map[new_i, new_j] != 1 and distances[new_i, new_j] > distances[current_node] + 1):
+        #             distances[new_i, new_j] = distances[current_node] + 1
+        #             queue.append((new_i, new_j))
+
+        # return distances
+        pass
+
+    def reset(self):
+        pass
